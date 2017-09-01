bf30f868c2b489a8299a27c3053da77b5b957b5e
Obtuse <theobtuseautodidact@gmail.com>
2017-09-01 15:42:55 -0700

    Add string_format method

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,14 +1,30 @@
 require 'csv'
 
+
 class CSVParser
 
     def parse(file ="spec/fixtures/single_line.csv" )
         handle = CSV.foreach(file, {:headers => true, header_converters: :symbol, encoding: 'iso-8859-1:utf-8'})
         handle.each do |row|
             puts(row.inspect)
+            puts(row[:zip])
+        end
+    end
+
+    def convert_time(time_string="") # default argument protects form wrong num of arguments error
+        # assume all dates are 20XX; compensate for two digit dates by adding 2000 years
+        # strptime didn't like "/"
+        time_string.gsub!("/","-")
+        begin
+            (DateTime.strptime(time_string, '%m-%d-%Y %l:%M:%S').next_year(2000) - (3.0/24.0)).iso8601()
+        rescue ArgumentError => e 
+            # TODO: Log error
+            puts("TODO: log (#{e}) to STDERR and do not record row")
         end
     end
 end
+
+
 
 
 # runner
--- spec/csv_parser_spec.rb
+++ spec/csv_parser_spec.rb
@@ -6,4 +6,21 @@ describe CSVParser do
         p = CSVParser.new
         expect(p.class).to eq(CSVParser)  
     end
+
+    it "should convert datetime string" do
+        p = CSVParser.new
+        expect(p.convert_time("10/2/04 8:44:11 AM")).to eq("2004-10-02T05:44:11+00:00")
+    end
+
+    it "should log an error if it fails to parse date" do
+        p = CSVParser.new
+        expect(p.convert_time("10/?/04 8:44:11 AM")).to eq(nil);
+
+    end
+
+    
+    skip "should log an error if it fails to parse date" do
+        p = CSVParser.new
+        expect(p.convert_time()).to eq(nil);
+    end
 end
